8bf2fa991f77d0d3d27ebd2cae864afe66bf74a3
gavin <gracecmpltd@163.com>
2018-11-20 19:00:10 +0800

    用户级别权限认证

--- src/main/java/com/fullcrum/common/LoginAuthorization.java
+++ src/main/java/com/fullcrum/common/LoginAuthorization.java
@@ -1,12 +1,14 @@
 package com.fullcrum.common;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -15,6 +17,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Aspect
 @Component
@@ -37,22 +41,57 @@ public class LoginAuthorization {
 		System.out.println("do authorization check before receiving htttp request");
 		System.out.println(request.getHeader("Authorization"));
 		String authTicket = request.getHeader("Authorization");
+		String uuid = request.getHeader("UUID");
 		System.out.println(authTicket);
 
 		System.out.println(request.getRequestURI());
 		if (request.getRequestURI().equals("/ppp/login") || request.getRequestURI().equals("/ppp/loginBySms") || request.getRequestURI().equals("/ppp/admin/login") || request.getRequestURI().equals("/ppp/msg/getUserMsg")
-				|| request.getRequestURI().equals("/ppp/resourceMarket/getAllInfo") || request.getRequestURI().equals("/ppp/resourceMarket/getCount") || request.getRequestURI().equals("/ppp/bills/filterbill")) {
+				|| request.getRequestURI().equals("/ppp/resourceMarket/getAllInfo") || request.getRequestURI().equals("/ppp/resourceMarket/getCount") || request.getRequestURI().equals("/ppp/bills/filterbill")|| request.getRequestURI().equals("/ppp/getValidatePic")) {
 			System.out.println("login page ......");
 		}else if(authTicket == null) {
 			System.out.println("authticket is null ............");
 			try {
-				response.getWriter().println("authnticket is nu");;
-				response.sendError(602, "Authorization cannot be null");
+				//response.getWriter().println("authnticket is nu");
+				response.sendError(602, "Authorization cannot be null,the request should take the Authorization token");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}else if (stringRedisTemplate.opsForValue().get(authTicket) != null) {
+		}else if (stringRedisTemplate.opsForValue().get(authTicket) == null) {
+			System.out.println("can not get authticket in redis ");
+			//response.addHeader("statusCode", "8088");
+			try {
+				response.sendError(702, "invalid token, token is wrong or is expired, cannot get token from backend");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(stringRedisTemplate.opsForValue().get(authTicket) != null){
+			
+			String auth_str = stringRedisTemplate.opsForValue().get(authTicket);
+			System.out.println(auth_str);
+			JSONObject parsejson = JSONObject.parseObject(auth_str);
+			if (!parsejson.getString("uuid").equals(uuid)) {
+				try {
+					response.sendError(802, "invalid token, token is invalid");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			Enumeration<String> ss =  request.getParameterNames();
+			System.out.println("print parameternames..................................................................");
+			
+			while (ss.hasMoreElements()) {
+				System.out.println(ss.nextElement());
+				
+			}
+			Enumeration<String> headn = request.getHeaderNames();
+			while(headn.hasMoreElements()) {
+				System.out.println(headn.nextElement());
+			}
 			stringRedisTemplate.expire(authTicket, 60, TimeUnit.SECONDS);
+		}else {
+			System.out.println("request ticket else ............................");
 		}
 		
 		//System.out.println(request.getAuthType());
@@ -84,4 +123,19 @@ public class LoginAuthorization {
 		
 		System.out.println(request.getCookies());*/
 	}
+	
+	
+	/*@After("LoginPointCut()")
+	public void AfterAuthentication(JoinPoint joinPoint) {
+		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpServletResponse response = attributes.getResponse();
+		if (response.getHeader("statusCode").equals("8088")) {
+			try {
+				response.sendError(777, "8088");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}*/
 }
